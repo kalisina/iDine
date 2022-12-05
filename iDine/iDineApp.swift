f1dbdf50882826389c804be61bf4c382ef5ffa20
Kali Sina <ali.sina89@yahoo.com>
2022-12-05 11:41:17 +0100

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Triumph on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
