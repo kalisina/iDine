7ddbd063729ecf6856ffa6c1460f5d08b09e992b
Kali Sina <ali.sina89@yahoo.com>
2022-12-05 12:13:57 +0100

    import project files from starter project

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack{
+            List {
+                Text("Hello world 1")
+                Text("Hello world 2")
+                Text("Hello world 3")
+            }
+            .navigationTitle("Menu")
+        }        
     }
 }
 
