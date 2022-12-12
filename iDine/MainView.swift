25a668ab2806a0d2c18a23094aa126a94fdd9c23
Kali Sina <ali.sina89@yahoo.com>
2022-12-12 22:56:56 +0100

    Create MainView and use a TabBar to display ContentView and OrderView

---
+++ iDine/MainView.swift
@@ -0,0 +1,31 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Triumph on 12/12/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+            
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+            .environmentObject(Order())
+    }
+}
