d630c173e9af2917b999fde94c0fd73b87ea5ac9
Kali Sina <ali.sina89@yahoo.com>
2022-12-23 15:11:49 +0100

    add NavigationStack inside OrderView

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -11,19 +11,21 @@ struct OrderView: View {
     @EnvironmentObject var order: Order
 
     var body: some View {
-        List {
-            Section {
-                ForEach(order.items) { item in
-                    HStack {
-                        Text(item.name)
-                        Spacer()
-                        Text("£\(item.price)")
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("£\(item.price)")
+                        }
                     }
                 }
-            }
-            Section {
-                NavigationLink("Place Order") {
-                    Text("Check out")
+                Section {
+                    NavigationLink("Place Order") {
+                        CheckoutView()
+                    }
                 }
             }
         }
