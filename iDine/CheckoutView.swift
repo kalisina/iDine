87dca33a817d1d0fd62b3d39dc83cd15740c4c9d
Kali Sina <ali.sina89@yahoo.com>
2022-12-23 15:55:52 +0100

    Add more sections and pickers to CheckoutView

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -12,14 +12,34 @@ struct CheckoutView: View {
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
     
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) { type in
                         Text(type)
                     }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine id", text: $loyaltyNumber)
+                }
+            }
+            Section("Add a tip?") {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) { tip in
+                        Text("\(tip)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Total: $100") {
+                Button("Confirm Order") {
                 }
             }
         }
